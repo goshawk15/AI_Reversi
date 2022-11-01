1daf4382d7476f6b1c99a22997caf3c67eb07154
nGoshawk15 <p3200077@aueb.gr>
2022-11-01 15:51:06 +0200

    basics

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        Game game = new Game(3,true);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Maximum depth?");
+        int ans0 = sc.nextInt();
+
+        System.out.println("Play first?(y/n)");
+        String ans1 = sc.next();
+
+        Game game = new Game(ans0,( (ans1 == "y") ? true : false));
         game.printBoard();
     }
 }
